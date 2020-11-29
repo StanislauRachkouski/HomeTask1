f02f4ce6186cfa9ab0b613b79c00079a15937833
StanislauRachkouski <StanislauRachkouski@users.noreply.github.com>
2020-11-29 04:39:16 +0300

    Добавьте файлы проекта.

---
+++ HomeTask1/Employee.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask1
+{
+    class Employee
+    {
+        int exp;
+        int skill;
+        public string surname;
+        string name;
+
+
+        public void setExp(int param)
+        {
+            this.exp = param;
+        }
+
+
+        public void setSkill(int param)
+        {
+            this.skill = param;
+            
+        }
+
+        public void setSurname(string param)
+        {
+            this.surname = param;
+        }
+
+        public void setName(string param)
+        {
+            this.name = param;
+
+        }
+
+        public string getVolume()
+        {
+            string empl = this.name + " " + this.surname;
+            return empl;
+
+        }
+       
+
+        
+        public int getBonusPercent()
+        {
+
+            int per=0;
+
+
+            if (this.exp < 1)
+            {
+               per = 0;
+                return per;
+            }
+            if (this.exp < 5)
+            {
+                per = 5;
+                return per;
+            }
+            if ((this.exp >= 5) && (this.skill < 3))
+            {
+                per = 10;
+                return per;
+            }
+            if ((this.exp > 5) && (this.skill >= 3) && (this.skill <= 4))
+            {
+                per = 15;
+                return per;
+            }
+            if ((this.exp > 5) && (this.skill == 5))
+            {
+                per = 20;
+                return per;
+            }
+
+            else Console.WriteLine("Vvedite vernie parametry");
+
+            return per;
+
+            
+
+
+        }
+
+
+    }
+
+}
+
+       
---
+++ HomeTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmployee = new Employee();
+            firstEmployee.setExp(4);
+            firstEmployee.setSkill(6);
+            firstEmployee.setSurname("Mike");
+            firstEmployee.setName("Nike");
+
+            
+            Console.WriteLine("Employee is  " + firstEmployee.getVolume());
+            Console.WriteLine("BonusPercent is  " + firstEmployee.getBonusPercent());
+        }
+    }
+}
